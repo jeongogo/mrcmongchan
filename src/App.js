abda36d8b909f4795d3beb6d70475d02fd18558b
hongchan <chcgogo1357@gmail.com>
2023-06-26 10:39:53 +0900

    code push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import RootStack from './screens/RootStack';
+import CodePush from 'react-native-code-push';
 
 const queryClient = new QueryClient();
 
@@ -15,4 +16,15 @@ function App() {
   );
 }
 
-export default App;
+const codePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+  updateDialog: { 
+    title: '...', 
+    optionalUpdateMessage: '...', 
+    optionalInstallButtonLabel: '업데이트', 
+    optionalIgnoreButtonLabel: '아니오.' 
+  },
+  installMode: CodePush.InstallMode.IMMEDIATE 
+}
+
+export default CodePush(codePushOptions)(App);
